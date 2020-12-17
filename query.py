e3fb8c9156d4aaf0b953dfbc665f8429940d008d
Timothy Arthur <timothya@archivesalberta.org>
2020-12-17 13:25:30 -0700

    Corrected errors in source (PAASH2020-explicit.txt)

--- query.py
+++ query.py
@@ -22,16 +22,3 @@ with open('labels.csv', 'w') as file:
 	writer.writerow(['Item', 'Label', 'altLabel'])
 	for row in labels:
 		writer.writerow(row)
-
-multi = g.query("""SELECT ?item (count(?label) as ?labelCount)
-	WHERE{
-	?item skos:prefLabel ?label .
-	}
-	GROUP BY ?item
-	""")
-
-with open('num_labels.csv', 'w') as file:
-	writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(['Item', 'Num_labels'])
-	for row in multi:
-		writer.writerow(row)
